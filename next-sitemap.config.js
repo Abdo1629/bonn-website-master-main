d2e61cd59861c64bedb095885c2590eb617545ef
Abdo1629 <abdelrahman.abdrabou1@gmail.com>
2025-11-05 00:44:01 +0200

    SiteMap Edit

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,29 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.bonnmed.com',
+  siteUrl: 'https://www.bonnmed.com', 
   generateRobotsTxt: true,
+  outDir: './public', 
   sitemapSize: 7000,
   changefreq: 'weekly',
   priority: 0.7,
-  exclude: ['/404'], 
+  exclude: ['/404', '/server-sitemap.xml', '/admin/*'],
+  
+  additionalPaths: async (config) => {
+    const paths = [
+      '/',
+      '/about',
+      '/services',
+      '/registration',
+      '/contact',
+      '/products[slug]',
+      'covix%20care',
+    ];
+
+    return paths.map((path) => ({
+      loc: path,
+      changefreq: 'weekly',
+      priority: 0.8,
+      lastmod: new Date().toISOString(),
+    }));
+  },
 };
